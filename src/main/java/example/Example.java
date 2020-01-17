0762340a637c34a9ae0b76d1b999bbf5ae4cd763
administrator <administrator@clearcorrect.com>
2020-01-17 15:42:21 +0530

    new java project

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -2,10 +2,11 @@ package example;
 
 import com.google.common.base.Joiner;
 
+import java.io.Console;
+
 public class Example {
 
     public static String join(String... args) {
         return Joiner.on(' ').join(args);
     }
-
 }
